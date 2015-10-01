d844ac35f5a450638de369a06337341d01ca2c8d
d43 <danaekhh@gmail.com>
2015-10-01 16:24:19 -0700

    Feature engineering added to preprocessing and database scripts

--- preprocessing_crime.py
+++ preprocessing_crime.py
@@ -8,7 +8,17 @@ from operator import itemgetter # for getting max tuple quickly
 def pre_process(cdf):
 	cdf.Date = pd.to_datetime(cdf.Date)
 	cdf['year'] = pd.DatetimeIndex(cdf.Date).year
-	cdf['year_month'] = cdf.Date.map(lambda x: 1000*x.year + x.month)
+
+	# Create time features
+	cdf['day_of_week'] = pd.DatetimeIndex(cdf.Date).day_of_week
+	cdf['hour'] = [i.hour for i in cdf.Time]
+	cdf['weekend'] = cdf.day_of_week.isin([5,6])*1
+	cdf['morning'] = cdf.hour.isin([1, 2, 3, 4, 5, 6, 7])*1
+	cdf['workday'] = cdf.hour.isin([8, 9, 10, 11, 12, 13, 14, 15])*1
+	cdf['evening'] = cdf.hour.isin([16, 17, 18, 19, 20, 21, 22, 23])*1
+	#cdf['year_month'] = cdf.Date.map(lambda x: 1000*x.year + x.month)
+
+	# Drop unused columns
 	cdf.drop(['Idx', 'OIdx', 'CType', 'Desc', 'Beat', 'Addr', 'Src', 'UCR', 'Statute'], axis=1, inplace=True)
 
 	# Remove two categories that don't trend well over time.
@@ -49,8 +59,12 @@ def pre_process(cdf):
 			l.append((row[0], row[6]))
 		l = [i[0] for i in l if not i == max(l, key=itemgetter(1))]
 		drop_me.extend(l)
-
 	cdf.drop(drop_me, inplace=True)
-	cdf_dummy = pd.get_dummies(cdf, prefix='CTYPE', columns=['CrimeCat'])
 
+	# Create dummy variables for crime category, hour, and day of week
+	cdf_dummy = pd.get_dummies(cdf, prefix='CTYPE', columns=['CrimeCat'])
+	cdf_dummy = pd.get_dummies(cdf_dummy, prefix='hr', columns=['hour'])
+	cdf_dummy = pd.get_dummies(cdf_dummy, prefix='dow', columns=['day_of_week'])
+
+	# Export to csv
 	cdf_dummy.to_csv("data/cdf.csv")
